6d15af20a3913e697bc6e4f0c9aefd3175132268
bestzy <bestzy6@qq.com>
2022-01-29 14:44:33 +0800

    add somethings

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("init ok")
+}
+
+//在训练营上，字节工程师并不建议使用init函数
+func init() {
+
+}
---
+++ server/router.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"ByteDanceCamp8th/server/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+// NewRouter 路由配置
+func NewRouter() *gin.Engine {
+	r := gin.Default()
+	gin.New()
+	r.Use(middleware.Cors())
+	// 路由
+	g := r.Group("/api/v1")
+
+	// 成员管理
+	g.GET("/member/find")
+	g.POST("/member/create")
+	g.GET("/member")
+	g.GET("/member/list")
+	g.POST("/member/update")
+	g.POST("/member/delete")
+
+	// 登录
+
+	g.POST("/auth/login")
+	g.POST("/auth/logout")
+	g.GET("/auth/whoami")
+
+	// 排课
+	g.POST("/course/create")
+	g.GET("/course/get")
+
+	g.POST("/teacher/bind_course")
+	g.POST("/teacher/unbind_course")
+	g.GET("/teacher/get_course")
+	g.POST("/course/schedule")
+
+	// 抢课
+	g.POST("/student/book_course")
+	g.GET("/student/course")
+	return r
+}
